42dc7ccccd99281d0f129becf81f9b2286d105f7
SyarifuddinSakri <syarifuddisakri@gmail.com>
2023-11-30 14:01:59 +0800

    removed unused lines

--- Core/Src/w5500/w5500_spi.c
+++ Core/Src/w5500/w5500_spi.c
@@ -4,30 +4,6 @@
 
 extern SPI_HandleTypeDef hspi1;
 
-//uint8_t SPIReadWrite(uint8_t data){
-//
-//	while ((hspi1.Instance->SR & SPI_FLAG_TXE)!=SPI_FLAG_TXE);
-//	*(__IO uint8_t*)&hspi1.Instance->DR=data;
-//
-//	while((hspi1.Instance->SR & SPI_FLAG_RXNE)!=SPI_FLAG_RXNE);
-//	 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-//	return(*(__IO uint8_t*)&hspi1.Instance->DR);
-//}
-//void SPIWrite(uint8_t data){
-//	while ((hspi1.Instance->SR & SPI_FLAG_TXE)==0){
-//		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
-//	}
-//	*(__IO uint8_t*)&hspi1.Instance->DR=data;
-//}
-//
-//uint8_t SPIRead(uint8_t data){
-//	while((hspi1.Instance->SR & SPI_FLAG_RXNE)!=SPI_FLAG_RXNE){
-//		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-//	}
-//
-//	return(*(__IO uint8_t*)&hspi1.Instance->DR);
-//}
-
 void wizchip_select(void){
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 }
@@ -61,46 +37,9 @@ void wizchip_write_burst(uint8_t* pBuf, uint16_t len) {
     }
 }
 
-//uint8_t wizchip_read(void){
-//	uint8_t rb;
-//	rb=SPIRead(0x00);//passing dummy data
-//	return rb;
-//}
-//
-//void wizchip_write(uint8_t wb){
-//	SPIWrite(wb);//writing a byte into the SPI Interface
-//}
-
-//void wizchip_readburst(uint8_t* pBuf, uint16_t len){
-//	for(int i=0; i<len;i++){
-//		*pBuf=SPIRead(0x00);
-//		pBuf++;
-//	}
-//}
-//void wizchip_writeburst(uint8_t* pBuf, uint16_t len){
-//	for(int i=0; i<len;i++){
-//		SPIWrite(*pBuf);
-//		pBuf++;
-//	}
-//}
-
-//void W5500IOInit(){
-//	GPIO_InitTypeDef GPIO_InitStruct = {0};
-//	__HAL_RCC_GPIOA_CLK_ENABLE();
-//
-//	GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1; //pin0 is clock reset and pin1 is chip select
-//	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-//	GPIO_InitStruct.Pull = GPIO_NOPULL;
-//	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-//
-//	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-//}
-
 void W5500Init(){
 	uint8_t tmp;
 	uint8_t memsize[2][8]={{2,2,2,2,2,2,2,2},{2,2,2,2,2,2,2,2}};
-
-//	W5500IOInit();
 
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);//Chip Select high by default
 
